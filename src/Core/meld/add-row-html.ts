80f92a6c00b54536393494e9d323e4755519c56a
Thomas Mays <thomas@mindofthomas.com>
2018-06-15 16:51:43 -0400

    rename hitRow to hit

--- src/Core/meld/add-row-html.ts
+++ src/Core/meld/add-row-html.ts
@@ -7,7 +7,7 @@ import { hidden } from '../../Interface/html/table';
 import makeButton from './make-button';
 
 export default function addRowHTML(
-	hitRow: ifc.HITData,
+	hit: ifc.HITData,
 	shouldHide: boolean,
 	reviewsError: boolean,
 	reviewsLoading: boolean,
@@ -16,47 +16,47 @@ export default function addRowHTML(
 	const center = 'text-align:center;';
 
 	let trClass = rowColor;
-	if (hitRow.included) trClass += ' includelisted';
+	if (hit.included) trClass += ' includelisted';
 	if (shouldHide) trClass += ' ignored hidden';
-	if (hitRow.blocked) trClass += ' blocklisted';
-	if (hitRow.shine) trClass += ' shine';
+	if (hit.blocked) trClass += ' blocklisted';
+	if (hit.shine) trClass += ' shine';
 
 	let pandaHref = '';
-	if (hitRow.hit.panda) pandaHref = `href="${hitRow.hit.panda}"`;
+	if (hit.hit.panda) pandaHref = `href="${hit.hit.panda}"`;
 
 	let requesterHref = '';
-	if (hitRow.requester.id) requesterHref = `href="${TO_REPORTS}${hitRow.requester.id}"`;
+	if (hit.requester.id) requesterHref = `href="${TO_REPORTS}${hit.requester.id}"`;
 
 	const expData = {
-		gid: hitRow.groupId,
+		gid: hit.groupId,
 	};
 
 	const titleTooltipData = {
-		data: hitRow,
+		data: hit,
 	};
 	const toTooltipData = {
 		error: reviewsError,
 		loading: reviewsLoading,
-		data: hitRow.TO,
+		data: hit.TO,
 	};
 
 	return cleanTemplate(`
 		<tr class="${trClass}">
-			<td class="block-tc ${hidden('block', hitRow.blocked)}">
-				<button name="block" value="${hitRow.requester.name}" class="block" title="Block this requester">
+			<td class="block-tc ${hidden('block', hit.blocked)}">
+				<button name="block" value="${hit.requester.name}" class="block" title="Block this requester">
 					R
 				</button>
-				<button name="block" value="${hitRow.title.replace(/"/g, '&quot;')}" class="block" title="Block this title">
+				<button name="block" value="${hit.title.replace(/"/g, '&quot;')}" class="block" title="Block this title">
 					T
 				</button>
-				<button name="block" value="${hitRow.groupId}" class="block" title="Block this Group ID">
+				<button name="block" value="${hit.groupId}" class="block" title="Block this Group ID">
 					ID
 				</button>
 			</td>
 			<td class="requester-tc ${hidden('requester')}">
 				<div>
-					<a class="static" target="_blank" href="${hitRow.requester.link}">
-						${hitRow.requester.name}
+					<a class="static" target="_blank" href="${hit.requester.link}">
+						${hit.requester.name}
 					</a>
 				</div>
 			</td>
@@ -69,31 +69,31 @@ export default function addRowHTML(
 					${makeButton('Pco', 'PC-O', 'Panda Crazy (Once)', expData, 'PC Once')}
 				</div>
 				<div>
-					<a target="_blank" class="static hit-title" href="${hitRow.hit.preview}">
-						${hitRow.title + createTooltip(titleTooltipData)}
+					<a target="_blank" class="static hit-title" href="${hit.hit.preview}">
+						${hit.title + createTooltip(titleTooltipData)}
 					</a>
 				</div>
 			</td>
 			<td class="rewardpanda-tc" style="${center}">
 				<a target="_blank" ${pandaHref}>
-					${hitRow.pay}
+					${hit.pay}
 				</a>
 			</td>
 			<td class="available-tc ${hidden('available')}" style="${center}">
-				${hitRow.numHits}
+				${hit.numHits}
 			</td>
 			<td class="duration-tc ${hidden('duration')}" style="${center}">
-				${hitRow.timeStr}
+				${hit.timeStr}
 			</td>
 			<td class="topay-tc ${hidden('topay')}" style="${center}">
-				<a class="static toLink" target="_blank" data-rid="${hitRow.requester.id || 'null'}" ${requesterHref}>
-					${(hitRow.TO ? hitRow.TO.attrs.pay : 'n/a') + createTooltip(toTooltipData)}
+				<a class="static toLink" target="_blank" data-rid="${hit.requester.id || 'null'}" ${requesterHref}>
+					${(hit.TO ? hit.TO.attrs.pay : 'n/a') + createTooltip(toTooltipData)}
 				</a>
 			</td>
-			<td style="${center}" class="${hitRow.masters ? 'reqmaster' : 'nomaster'} masters-tc ${hidden('masters')}"">
-				${hitRow.masters ? 'Y' : 'N'}
+			<td style="${center}" class="${hit.masters ? 'reqmaster' : 'nomaster'} masters-tc ${hidden('masters')}"">
+				${hit.masters ? 'Y' : 'N'}
 			</td>
-			<td class="tooweak notqualified-tc ${hidden('notqualified', hitRow.qualified)}" title="Not qualified to work on this HIT">
+			<td class="tooweak notqualified-tc ${hidden('notqualified', hit.qualified)}" title="Not qualified to work on this HIT">
 				NQ
 			</td>
 		</tr>
