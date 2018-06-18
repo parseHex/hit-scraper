07083f4edc209b6b1a3fb5cc73b8ecaa3ad31333
Thomas Mays <thomas@mindofthomas.com>
2018-06-18 18:09:12 -0400

    change method of start/stop

--- src/api/start-stop.ts
+++ src/api/start-stop.ts
@@ -1,12 +1,20 @@
-import Interface from 'Interface';
+import Core from 'Core';
 
 export function start() {
-	if (Interface.buttons.main.textContent === 'Stop') return;
+	const mainBtn = <HTMLButtonElement>document.getElementById('main');
 
-	Interface.buttons.main.click();
+	if (mainBtn.textContent === 'Stop') return;
+
+	Core.cruising = true;
+	mainBtn.textContent = 'Stop';
+	Core.run();
 }
 export function stop() {
-	if (Interface.buttons.main.textContent === 'Start') return;
+	const mainBtn = <HTMLButtonElement>document.getElementById('main');
 
-	Interface.buttons.main.click();
+	if (mainBtn.textContent === 'Start') return;
+
+	Core.cruising = false;
+	mainBtn.textContent = 'Start';
+	Core.run();
 }
