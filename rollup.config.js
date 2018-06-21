795ff4e599e4226fe9fe58c812db6e71fb1c5aa8
Thomas Mays <thomas@mindofthomas.com>
2018-06-21 04:50:19 -0400

    update/fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,28 +3,22 @@ const path = require('path');
 const typescript = require('rollup-plugin-typescript2');
 
 function header(section, name) {
-	return new Promise(function (resolve) {
-		fs.readFile(path.resolve(__dirname, 'src/_header.ts'), {
-			encoding: 'utf-8',
-		}, function (err, data) {
-			if (err) throw err;
-
-			resolve(data);
-		});
+	fs.readFileSync(path.resolve(__dirname, 'src/_header.ts'), {
+		encoding: 'utf-8',
 	});
 }
 
 export default {
 	input: 'src/index.ts',
 	output: {
+		name: 'hit_scraper',
+		banner: header(),
 		file: 'build/hit-scraper.user.js',
 		format: 'iife',
 	},
-	banner: header(),
 	watch: {
 		exclude: ['node_modules/**']
 	},
-	name: 'hit_scraper',
 	plugins: [
 		typescript(/*{ plugin options }*/),
 	],
