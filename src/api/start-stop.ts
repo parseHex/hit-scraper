31141a259e4f4514ee7256f9cc6e8d23534f3e4f
Thomas Mays <thomas@mindofthomas.com>
2018-06-25 21:39:25 -0400

    back to clicking button to start/stop

--- src/api/start-stop.ts
+++ src/api/start-stop.ts
@@ -1,20 +1,10 @@
-import Core from 'Core';
-
 export function start() {
 	const mainBtn = <HTMLButtonElement>document.getElementById('main');
 
-	if (mainBtn.textContent === 'Stop') return;
-
-	Core.cruising = true;
-	mainBtn.textContent = 'Stop';
-	Core.run();
+	mainBtn.click();
 }
 export function stop() {
 	const mainBtn = <HTMLButtonElement>document.getElementById('main');
 
-	if (mainBtn.textContent === 'Start') return;
-
-	Core.cruising = false;
-	mainBtn.textContent = 'Start';
-	Core.run();
+	mainBtn.click();
 }
