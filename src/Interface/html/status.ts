04b6b341e5051da8bdd378afd0f0e4f890c64783
Thomas Mays <thomas@mindofthomas.com>
2018-06-25 21:39:38 -0400

    typo in status messages

--- src/Interface/html/status.ts
+++ src/Interface/html/status.ts
@@ -37,10 +37,10 @@ export default `
 			&nbsp; seconds
 		</p>
 		<p id="status-scrape-error" class="hidden">
-			Error scrapng MTurk. Check console for details.
+			Error scraping MTurk. Check console for details.
 		</p>
 		<p id="status-scrape-error-disabled-search" class="hidden">
-			Error scrapng MTurk. Check console for details. Turned off Auto-Refresh.
+			Error scraping MTurk. Check console for details. Turned off Auto-Refresh.
 		</p>
 	</div>
 `;
