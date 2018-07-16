38574fe505dbc96ecccd287c6859c55a3d3e697f
Thomas Mays <thomas@mindofthomas.com>
2018-07-16 17:24:13 -0400

    fix api start/stop

--- src/api/start-stop.ts
+++ src/api/start-stop.ts
@@ -1,10 +1,14 @@
 export function start() {
 	const mainBtn = <HTMLButtonElement>document.getElementById('main');
 
+	if (mainBtn.textContent === 'Stop') return;
+
 	mainBtn.click();
 }
 export function stop() {
 	const mainBtn = <HTMLButtonElement>document.getElementById('main');
+
+	if (mainBtn.textContent === 'Start') return;
 
 	mainBtn.click();
 }
