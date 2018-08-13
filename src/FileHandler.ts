bd6afcacab0069de825d98f0d78a1aaccbdcd2d7
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2018-08-13 16:55:18 -0400

    refactor FileHandler

--- src/FileHandler.ts
+++ src/FileHandler.ts
@@ -1,17 +1,24 @@
 import Settings from 'Settings';
-import { IGNORE_KEY, INCLUDE_KEY } from 'lib/constants';
+import { IGNORE_KEY, INCLUDE_KEY, SETTINGS_KEY } from 'lib/constants';
 
 import initialize from './index';
 
+interface SettingsObject {
+	settings: string; // SettingsConfig
+	ignore_list: string;
+	include_list: string;
+}
+
 export default class FileHandler {
 	static exports() {
-		var obj = {
+		const obj: SettingsObject = {
 			settings: JSON.stringify(Settings.user),
 			ignore_list: localStorage.getItem(IGNORE_KEY) || '',
 			include_list: localStorage.getItem(INCLUDE_KEY) || ''
 		};
-		var blob = new Blob([JSON.stringify(obj)], { type: 'application/json' });
-		var a = document.body.appendChild(document.createElement('a'));
+
+		const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' });
+		const a = document.body.appendChild(document.createElement('a'));
 		a.href = URL.createObjectURL(blob);
 		a.download = 'hitscraper_settings.json';
 		a.click();
@@ -19,20 +26,37 @@ export default class FileHandler {
 	}
 	static imports(e: Event) {
 		const target = <HTMLInputElement>e.target;
-		var f = target.files;
-		var invalid = () => Settings.mainEl.querySelector('#eisStatus').textContent = 'Invalid file.';
-		if (!f.length) return;
-		if (!f[0].name.includes('json')) return invalid();
-		var reader = new FileReader();
-		reader.readAsText(f[0]);
-		reader.onload = function () {
-			var obj;
-			try { obj = JSON.parse(this.result); } catch (err) { return invalid(); }
-			for (var key of ['settings', 'ignore_list', 'include_list']) {
-				if (key in obj && typeof obj[key] === 'string')
-					localStorage.setItem(IGNORE_KEY.replace('ignore_list', key), obj[key]);
+		const files = target.files;
+
+		if (!files.length) return;
+		if (!files[0].name.includes('json')) {
+			return invalid();
+		}
+
+		const reader = new FileReader();
+		reader.readAsText(files[0]);
+		reader.addEventListener('load', function () {
+			const result = <string>reader.result;
+			let obj: Partial<SettingsObject>;
+			try {
+				obj = JSON.parse(result);
+			} catch (err) {
+				return invalid();
+			}
+
+			if (obj.ignore_list) {
+				localStorage.setItem(IGNORE_KEY, obj.ignore_list);
+			}
+			if (obj.include_list) {
+				localStorage.setItem(INCLUDE_KEY, obj.include_list);
+			}
+			if (obj.settings) {
+				localStorage.setItem(SETTINGS_KEY, obj.settings);
 			}
 			initialize();
-		};
+		});
 	}
+}
+function invalid() {
+	Settings.mainEl.querySelector('#eisStatus').textContent = 'Invalid file.';
 }
