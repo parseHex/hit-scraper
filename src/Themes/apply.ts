4a3489d02cf5361f19e0ca3f012f67d8336ae2b3
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2018-08-20 01:09:07 -0400

    remove Themes

--- src/Themes/apply.ts
+++
@@ -1,9 +0,0 @@
-import { Themes } from './index';
-
-export default function (this: Themes, theme: string, mode?: string) {
-	const cssNew = URL.createObjectURL(new Blob([this.generateCSS(theme, mode)], { type: 'text/css' }));
-	const rel = <HTMLLinkElement>document.head.querySelector('link[rel=stylesheet]');
-	const cssOld = rel.href;
-	rel.href = cssNew;
-	URL.revokeObjectURL(cssOld);
-}
--- src/Themes/get-brightness.ts
+++
@@ -1,5 +0,0 @@
-export default function (hex: string) {
-	// TODO: put in Colors object
-	var r = parseInt(hex.slice(1, 3), 16), g = parseInt(hex.slice(3, 5), 16), b = parseInt(hex.slice(5, 7), 16);
-	return ((r * 299) + (g * 587) + (b * 114)) / 1000;
-}
